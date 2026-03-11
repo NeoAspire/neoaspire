39a4904093f95cae335c2f528338a4e53380e0b8
neoaspire <neoaspire9@gmail.com>
2026-03-11 21:30:11 +0530

    Dashboard Exam Management System Live

---
+++ ems/assests/js/menu-toggle.js
@@ -0,0 +1,10 @@
+// Toggle navigation menu Humburger
+
+const menuToggle = document.getElementById("menuToggle");
+const emsNav = document.getElementById("emsNav");
+
+menuToggle.addEventListener("click", function(){
+    emsNav.classList.toggle("show");
+});
+
+
